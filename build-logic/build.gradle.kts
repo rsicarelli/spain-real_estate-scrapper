be3cba89906edae4e7b47bcfcfaadb78a49f23ff
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2022-03-31 10:40:53 +0200

    fix gitignore

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
     implementation(libs.kotlin.gradle)
     implementation(libs.gradle.ksp)
     implementation(libs.kotlin.serialization)
-//    implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.1.2")
 }
 
 tasks.withType(KotlinCompile::class.java).all {
